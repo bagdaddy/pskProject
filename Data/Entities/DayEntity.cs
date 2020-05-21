fc1e2f9f4a6b2e6cbce8f7726a40b1110525ce60
Universitetotomas <viesastomas@gmail.com>
2020-05-21 23:50:31 +0300

    Pridetas dienos entity

---
+++ Data/Entities/DayEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP.Data.Entities
+{
+    public class DayEntity
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeesId { get; set; }
+        public DateTime Date { get; set; }
+        public List<Subject> SubjectList { get; set; }
+    }
+}
