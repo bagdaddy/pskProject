c2b8cd9feca59b12fc5f962feaf11b2dfba4529e
mantasb <mb@frontit.dk>
2020-05-23 00:21:56 +0300

    finish working graph with fake data

--- ClientApp/src/components/LearningTree.js
+++ ClientApp/src/components/LearningTree.js
@@ -1,52 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import Tree from 'react-d3-tree';
+import { data } from './fakeData.js';
 
-const styles = {
-    links: {
-
-    },
-    nodes: {
-        node: {
-            circle: {},
-            name: {
-                border: 1,
-            },
-            attributes: {},
-        },
-        leafNode: {
-            circle: {},
-            name: {},
-            attributes: {},
-        },
-    },
-}
-const rect = {
-    shape: 'rect',
-    shapeProps: {
-        width: 20,
-        height: 20,
-        x: -10,
-        y: -10,
-        fill: 'red',
-    }
-};
 const circle = {
     shape: 'circle',
     shapeProps: {
-        r: 8,
+        r: 10,
         fill: 'grey',
     }
+};
+
+const circleLearntByYou = {
+    shape: 'circle',
+    shapeProps: {
+        r: 10,
+        fill: '#3CB043',
+    }
+};
+
+const circleLearntByTeam = {
+    shape: 'circle',
+    shapeProps: {
+        r: 10,
+        fill: '#2832C2',
+    }
 }
+
+const authenticatedUserId = "1";
+
+const SubjectInfo = props => {
+    let subjectData = props.data;
+    if (Object.keys(subjectData).length > 0) {
+        if (parseInt(subjectData.attributes.subjectId) === -1) {
+            return (
+                <div></div>
+            )
+        }
+    } else {
+        return (
+            <div></div>
+        )
+    }
+
+    if (Object.keys(props.data).length > 0 && subjectData.attributes.employees.length > 0) {
+        return (
+            <div className="subjectInfo">
+                <p className="title">{subjectData.name}</p>
+                <ul className="employeeList">
+                    {subjectData.attributes.employees.map(employee => (
+                        <li><a href={"/profile?id=" + employee.id}>{employee.name}</a></li>
+                    ))}
+                </ul>
+            </div>
+        )
+    } else {
+        return (
+            <div className="subjectInfo">
+                <p className="title">{subjectData.name}</p>
+                <div className="info">
+                    <p>Your team has yet to learn this subject.</p>
+                </div>
+            </div>
+        )
+    }
+
+
+};
 
 class NodeLabel extends React.PureComponent {
     render() {
         const { className, nodeData } = this.props
         return (
             <div className={className}>
-                <div className="numOfEmployees red">1</div>
                 <div className="subjectName">
                     <p>{nodeData.name}</p>
-                    </div>
+                </div>
+
             </div>
         )
     }
@@ -56,7 +85,8 @@ const textProps = { x: -20, y: 20 };
 const LearningTree = props => {
 
     const [subjects, setSubjects] = useState([]);
-
+    const [treeData, setTreeData] = useState([]);
+    const [displayedNode, setDisplayedNode] = useState({});
     const fetchData = React.useCallback(() => {
         fetch("api/GetSubjects")
             .then(response => response.json())
@@ -67,49 +97,77 @@ const LearningTree = props => {
     });
 
     useEffect(() => {
-        fetchData()
+        fetchData();
+        let myFakeTreeData = [
+            {
+                name: "Top level",
+                nodeSvgShape: circle,
+                attributes: {
+                    subjectId: "-1"
+                },
+                children: []
+            }
+        ];
+
+        function formSubjectObj(subject) {
+            let selectedNodeSvgShape = circle;
+            if (subject.employees.length > 0) {
+                if (subject.employees.filter(e => e.id === authenticatedUserId).length > 0) {
+                    selectedNodeSvgShape = circleLearntByYou;
+                } else {
+                    selectedNodeSvgShape = circleLearntByTeam;
+                }
+            }
+
+            let subjectToPush = {
+                name: subject.name,
+                nodeSvgShape: selectedNodeSvgShape,
+                attributes: {
+                    subjectId: subject.id,
+                    employees: subject.employees
+                },
+                children: getChildren(subject)
+            };
+            return subjectToPush;
+        }
+
+        function getChildren(subjectData) {
+            let children = [];
+            subjectData.subjects.forEach(subject => {
+                let subjectToPush = formSubjectObj(subject);
+                children.push(subjectToPush);
+            });
+            return children;
+        }
+
+        data.forEach(subject => {
+            let subjectToPush = formSubjectObj(subject);
+            myFakeTreeData[0].children.push(subjectToPush);
+        });
+        setTreeData(myFakeTreeData);
     }, []);
-    const myTreeData = [
-        {
-            name: 'Top lvl',
-            nodeSvgShape: circle,
-            children: [
-                {
-                    name: 'Java',
-                    attributes: {
-                        subjectId: 1,
-                    },
-                    nodeSvgShape: circle,
-                    children: [
-                        {
-                            name: 'Java8',
-                            attributes: {
-                                subjectId: "3",
 
-                            },
-                        }
-                    ]
-                },
-                {
-                    name: '.NET',
-                },
-            ],
-        },
-    ];
+    console.log(treeData);
 
-    const handleClick = (event, node) => {
+
+    const handleClick = React.useCallback((event, node) => {
         console.log('handle click ', event);
         console.log('handle click node', node);
-    }
+        setDisplayedNode(event);
+    });
 
     return (
-        <div style={{ width: "100%", height: "1000px" }}>
-            <Tree data={myTreeData} collapsible={false} onClick={handleClick} allowForeignObjects styles={styles} nodeLabelComponent={{
+        <div className="treeWrapper" style={{ width: "100%", height: "1000px" }}>
+            <SubjectInfo data={displayedNode} />
+            {treeData.length > 0 && <Tree data={treeData} collapsible={false} onClick={handleClick} allowForeignObjects transitionDuration={0} nodeLabelComponent={{
                 render: <NodeLabel className='myLabelComponentInSvg' />,
                 foreignObjectWrapper: {
-                    y: 0
+                    y: 0,
+                    x: 0
                 }
-            }} />
+            }}
+                nodeSize={{ x: 350, y: 70 }} />
+            }
         </div>
     );
 }
