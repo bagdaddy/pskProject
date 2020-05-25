73e9a011627a28966357b46a10ba135b6a752fc9
mantasb <mb@frontit.dk>
2020-05-25 18:46:53 +0300

    finish up subject insertion form

--- ClientApp/src/components/AddSubject.js
+++ ClientApp/src/components/AddSubject.js
@@ -1,17 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 const AddSubject = props => {
-    let subjectName = "";
+    const [subjects, setSubjects] = useState([]);
+    const [insertedSubject, setInsertedSubject] = useState({});
 
-    const handleChange = (event) => {
-        console.log(event.target.value);
-        subjectName = event.target.value;
+    const fetchSubjects = React.useCallback(() => {
+        fetch('api/GetSubjects')
+            .then(response => response.json())
+            .then(data => setSubjects(data));
+    });
+
+    const insertSubject = () => {
+        let parentSubjetcId = document.getElementById("parent").value !== "-1" ? document.getElementById("parent").value : null;
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                Name: document.getElementById("subject_name").value,
+                Description: document.getElementById("description").value,
+                ParentSubjectId: parentSubjetcId
+            })
+        };
+        console.log(requestOptions);
+        fetch('api/CreateSubject', requestOptions)
+            .then(response => response.text())
+            .then(data => setInsertedSubject(data));
     };
+
+    useEffect(() => {
+        fetchSubjects();
+    }, []);
 
     const handleSubmit = (event) => {
-        console.log(subjectName)
         event.preventDefault();
+        insertSubject();
     };
+
+    const getChildren = (subject) => {
+        return(
+            <
+        )
+    }
+
+    console.log(subjects);
+
 
     return (
         <div className="form-left">
@@ -21,12 +53,16 @@ const AddSubject = props => {
                     <Input type="text" id="subject_name" name="subject_name" placeholder='e.g. "PHP"' />
                 </FormGroup>
                 <FormGroup>
+                    <Label for="description">Description</Label>
+                    <Input type="text" id="description" name="description" placeholder="A really important subject" />
+                </FormGroup>
+                <FormGroup>
                     <Label for="parent_subject">Parent</Label>
                     <Input type="select" name="parent" id="parent">
                         <option value="-1">-</option>
-                        <option value="1">PHP</option>
-                        <option value="2">C#</option>
-                        <option value="3">Java</option>
+                        {subjects.length > 0 && subjects.map(subject => (
+                            <option key={subject.id} value={subject.id}>{subject.name}</option>
+                        ))}
                     </Input>
                 </FormGroup>
                 <FormGroup>
@@ -34,7 +70,7 @@ const AddSubject = props => {
                 </FormGroup>
             </Form>
         </div>
-        
+
     );
 
 };
