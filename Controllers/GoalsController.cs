2dcaad84b8cd44710b4cbece1c566ebaa6b98c26
universitetotomas <viesastomas@gmail.com>
2020-05-25 16:39:09 +0300

    Added Goal Entity and GoalsController

---
+++ Controllers/GoalsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TP.Data.Entities;
+
+namespace TP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalsController
+    {
+        private List<Goal> list;
+        public GoalsController()
+        {
+            list = new List<Goal> {
+                new Goal(new Guid("b96f7695-c9d9-4b5c-849e-4219083d6220"),new Guid("819df009-1d4d-4a57-ac73-55e79973992a")),
+                new Goal(new Guid("b96f7695-c9d9-4b5c-849e-4219083d6220"),new Guid("ba099e58-9194-4841-b047-5d0c4541bba5")),
+                new Goal(new Guid("1c8c9ce0-2251-4ef3-ab0b-cfc7dd7a2949"),new Guid("5c478ca2-b93f-4535-adc2-14676398d93e")),
+            };
+        }
+    }
+}
---
+++ Data/Entities/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TP.Data.Entities
+{
+    public class Goal
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeesId { get; set; }
+        public Guid SubjectId { get; set; }
+
+        public Goal(Guid employeesid, Guid subjectId)
+        {
+            Id = Guid.NewGuid();
+            EmployeesId = employeesid;
+            SubjectId = subjectId;
+        }
+    }
+}
