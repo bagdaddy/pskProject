68921b199eaefdc5e987c0362c21aefeba797cf1
universitetotomas <viesastomas@gmail.com>
2020-05-25 16:58:58 +0300

    Added methods to GoalsController

--- Controllers/GoalsController.cs
+++ Controllers/GoalsController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TP.Data.Entities;
 
@@ -18,5 +20,28 @@ namespace TP.Controllers
                 new Goal(new Guid("1c8c9ce0-2251-4ef3-ab0b-cfc7dd7a2949"),new Guid("5c478ca2-b93f-4535-adc2-14676398d93e")),
             };
         }
+        //get all by workder id
+        [HttpGet("{workerId}")]
+        public async Task<IActionResult> getAll(Guid employeeid)
+        {
+            var workerGoals = list.Where(x => x.EmployeesId == employeeid).ToList();
+            if (workerGoals.Count != 0) { return Ok(workerGoals); }
+            return BadRequest("oopsie");
+        }
+        //get signle by object id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getSingle(Guid id)
+        {
+            var goalId = list.Select(x => x.Id).ToList();
+            if (goalId.Contains(id)) { return Ok(list.First(x => x.Id == id)); }
+            return BadRequest("oopsie");
+        }
+
+        [HttpPost("{workerId}/{subjectId}")]
+        public async Task<IActionResult> AddGoal(Guid employeesId,Guid subjectId)
+        {
+            list.Add(new Goal(employeesId,subjectId));
+        }
     }
+
 }
