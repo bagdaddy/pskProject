c213df3314d3354e5c63640ff6c7ffb431129ef8
Justaslol <ko.justas@gmail.com>
2020-05-26 17:01:31 +0300

    Updated version

--- Startup.cs
+++ Startup.cs
@@ -91,7 +91,7 @@ namespace TP
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                //c.RoutePrefix = string.Empty;
             });
         }
     }
