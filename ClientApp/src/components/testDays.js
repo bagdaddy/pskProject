4ce0fa86ac7d45d562ada9cc484bd19632806698
intager S <jmal@inbox.lt>
2020-05-27 23:38:25 +0300

    Added preliminary restrictions to creatin/changing/deleting dates

---
+++ ClientApp/src/components/testDays.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 5,
+        date: new Date(2020, 3, 10),
+        subjects: [4, 4, null, null],
+        comment: 'ExampleComment',
+    },
+    {
+        id: 2,
+        date: new Date(2020, 5, 11),
+        subjects: [1, null, null, null],
+        comment: 'ExampleComment',
+    },
+    {
+        id: 3,
+        date: new Date(2020, 4, 20),
+        subjects: [1, 2, 3, 4],
+        comment: 'ExampleComment',
+    }
+]
---
+++ ClientApp/src/components/testSubjects.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        id: 1,
+        title: 'ExampleSubject1',
+    },
+    {
+        id: 2,
+        title: 'ExampleSubject2',
+    },
+    {
+        id: 3,
+        title: 'ExampleSubject3',
+    },
+    {
+        id: 4,
+        title: 'ExampleSubject4',
+    }
+]
