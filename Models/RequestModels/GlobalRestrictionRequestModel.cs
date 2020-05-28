129718c625ead773995e32479644980c35d5f586
MatasKirvaitis <kirvaitism@gmail.com>
2020-05-28 20:53:48 +0300

    Update request model

--- Models/RequestModels/GlobalRestrictionRequestModel.cs
+++ Models/RequestModels/GlobalRestrictionRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace TP.Models.RequestModels
 {
     public class GlobalRestrictionRequestModel
     {
-        public int GlobalDayLimit { get; set; }
+        [Required]
+        public int? GlobalDayLimit { get; set; }
     }
 }
