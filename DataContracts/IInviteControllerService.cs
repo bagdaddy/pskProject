12797b93f6e2387c708c8a1bd9292cda17a64c25
MatasKirvaitis <kirvaitism@gmail.com>
2020-05-29 12:36:14 +0300

    Added delete invite service

--- DataContracts/IInviteControllerService.cs
+++ DataContracts/IInviteControllerService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TP.Data.Entities;
 using TP.Models.RequestModels;
 
 namespace TP.DataContracts
@@ -10,5 +11,8 @@ namespace TP.DataContracts
     public interface IInviteControllerService
     {
         public Task<Guid> CreateInvite(Guid id, EmailRequestModel model);
+        public Task DeleteInvite(Guid id);
+        public Task<Invite> GetInvite(Guid id);
+
     }
 }
